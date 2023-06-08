0c5f6345581dda86995297bf6f4eb25b1af37bde
Victor200107 <victor.beltranvillalba@gmail.com>
2023-06-07 19:35:00 -0500

    primera version

---
+++ Beltran Villalba/Beltran Villalba/Models/AplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Beltran_Villalba.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Beltran_Villalba.Models
+
+{
+
+    public class AplicationDbContext: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // El connectionString debe venir de un archivo de configuraciones
+            optionsBuilder.UseSqlServer("Server=VICTOR07\\SQLEXPRESS;Database=BeltranVillalba;Trusted_Connection=True")
+                .EnableSensitiveDataLogging(true)
+                .UseLoggerFactory(LoggerFactory.Create(builder =>
+                {
+                    builder.AddConsole().SetMinimumLevel(LogLevel.Information);
+                }));
+        }
+        public DbSet<Usuario> usuarios { get; set; }
+        public DbSet<Genero> generos { get; set; }
+
+    }
+}
